feee5f7b5bde89ab1eff8f69ddd1dafc9233c6b7
GrozaMorei <vitalik2004levcov@gmail.com>
2024-10-15 16:38:24 +0300

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <article>
       <section>
-        <h1>Привет, мир!)</h1>
+        <h1>Привет, мир!</h1>
         <p>Как ваши дела?</p>
       </section>
     </article>
