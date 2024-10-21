0c891d6e09e3537211af64eede3a0181eac42f49
ISeb-Skv <ilya.skvortsov.2004@mail.ru>
2024-10-21 21:41:56 +0300

    add: slider adapt

--- src/app/components/PromotionalProducts.js
+++ src/app/components/PromotionalProducts.js
@@ -1,4 +1,6 @@
 import '../../styles/components/PromotionalProducts.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css"; 
 
 import ProductCard from '../components/ProductCard';
 
@@ -9,13 +11,13 @@ import Slider from 'react-slick';
 import { useEffect, useState } from 'react';
 
 const PromotionalProducts = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(true);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 740);
+      setIsMobile(window.innerWidth < 1920);
 
-      console.log(`Is mobile: ${window.innerWidth < 740}`);
+      console.log(`Is mobile: ${window.innerWidth < 1920}`);
     };
 
     handleResize(); // Проверяем при первой загрузке
@@ -27,6 +29,7 @@ const PromotionalProducts = () => {
   }, []);
 
   const sliderSettings = {
+
     dots: true,
     infinite: true,
     speed: 500,
@@ -41,15 +44,19 @@ const PromotionalProducts = () => {
           <span>АКЦИОННЫЕ</span> ТОВАРЫ
         </h2>
 
-        {isMobile ? (
-          <Slider {...sliderSettings}>
-            {products.map((product) => (
-              <div key={product.id}>
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </Slider>
-        ) : (
+        {isMobile && (
+          <div className='promo-products__slider'>
+            <Slider {...sliderSettings}>
+                {products.map((product) => (
+                <div key={product.id}>
+                  <ProductCard product={product} />
+                </div>
+              ))}
+            </Slider>
+          </div>
+          
+        )}
+        {!isMobile && (
           <ul className="promo-products__list">
             {products.map((product) => (
               <li key={product.id}>
